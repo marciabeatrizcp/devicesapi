73b587af32599250862985d93ca8f32dced2af90
Beatriz Domingues <marciabeatrizcp@gmail.com>
2022-10-28 17:48:34 -0300

    fix: app name in conection parameters

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :devices_api, DevicesAPI.Repo,
   database: "devices_api_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  parameters: [application_name: "devices_api"]
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
