0120b64e87838bdd687ffac4ec1147b17a95c667
Beatriz Domingues <marciabeatrizcp@gmail.com>
2022-11-14 15:59:29 -0300

    chore: create a default schema

---
+++ lib/devices_api/schema.ex
@@ -0,0 +1,12 @@
+defmodule DevicesApi.Schema do
+  @moduledoc """
+  Defines DeviceAPI schema defaults.
+  """
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+    end
+  end
+end
