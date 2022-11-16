9049e925eabcf8766f75f381f387c0d2995675eb
Beatriz Domingues <marciabeatrizcp@gmail.com>
2022-11-16 16:06:29 -0300

    fix: credo and formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,7 @@
+# Used by "mix format"
 [
   import_deps: [:ecto, :phoenix],
+  line_length: 100,
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
 ]
