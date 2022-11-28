535bf4c9b7acc7cc50086728a76e4c3b32c18427
Beatriz Domingues <marciabeatrizcp@gmail.com>
2022-11-28 11:01:05 -0300

    fix: format

--- lib/devices_api/users/commands/create.ex
+++ lib/devices_api/users/commands/create.ex
@@ -26,11 +26,11 @@ defmodule DevicesApi.Users.Commands.Create do
   """
   @spec execute(user_params()) :: {:error, Ecto.Changeset.t()} | {:ok, Ecto.Schema.t()}
   def execute(params) when is_map(params) do
-    with %Ecto.Changeset{valid?:  true} = changeset <- User.changeset(params),
+    with %Ecto.Changeset{valid?: true} = changeset <- User.changeset(params),
          {:ok, user} <- Repo.insert(changeset) do
       {:ok, user}
     else
-      %Ecto.Changeset{valid?:  false} = changeset -> {:error, changeset}
+      %Ecto.Changeset{valid?: false} = changeset -> {:error, changeset}
       {:error, _} = err -> err
     end
   end
