edf51b57148e75ff0b29add689df8cdc9f2aedc4
Beatriz Domingues <marciabeatrizcp@gmail.com>
2022-11-28 11:30:49 -0300

    fix: hex.outdated

--- mix.exs
+++ mix.exs
@@ -42,16 +42,16 @@ defmodule DevicesAPI.MixProject do
     [
       {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.9.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.6"},
-      {:argon2_elixir, "~> 2.0"},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.10", only: :test},
+      {:argon2_elixir, "~> 3.0"},
+      {:ex_doc, "~> 0.29.1", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.15.1", only: :test},
       {:credo, "~> 1.6.2", only: [:dev, :test], runtime: false}
     ]
   end
