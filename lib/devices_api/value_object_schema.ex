4e4b6961ad42c47aaceb38ab8ba85317f2974265
Beatriz Domingues <marciabeatrizcp@gmail.com>
2022-11-29 17:01:28 -0300

    Update lib/devices_api/value_object_schema.ex
    Co-authored-by: Victor Oliveira Nascimento <376386+victorolinasc@users.noreply.github.com>

--- lib/devices_api/value_object_schema.ex
+++ lib/devices_api/value_object_schema.ex
@@ -1,6 +1,7 @@
 defmodule DevicesApi.ValueObjectSchema do
   @moduledoc """
   Models a value object.
+  
   A value object is one that has no identity. It is mostly used as an embedded object.
   """
   defmacro __using__(_) do
