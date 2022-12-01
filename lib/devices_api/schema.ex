58badff2325b1c3826b97a1c02e51a8a9eefe0fa
Beatriz Domingues <marciabeatrizcp@gmail.com>
2022-12-01 17:25:12 -0300

    fix: dialyzer

--- lib/devices_api/schema.ex
+++ lib/devices_api/schema.ex
@@ -7,6 +7,8 @@ defmodule DevicesApi.Schema do
       use Ecto.Schema
       @primary_key {:id, :binary_id, autogenerate: true}
       @foreign_key_type :binary_id
+
+      @type t() :: %__MODULE__{}
     end
   end
 end
